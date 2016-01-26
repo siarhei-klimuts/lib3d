9662ebd529e5e2cec65d83122753890863971297
Siarhei Klimuts <galiaf47@gmail.com>
2016-01-26 11:53:35 +0300

    Configured tests coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,5 @@
 var webpackConfig = require('./webpack.config.js');
+var path = require('path');
 
 module.exports = function(config) {
     config.set({
@@ -16,10 +17,24 @@ module.exports = function(config) {
         },
         webpack: {
             devtool: 'inline-source-map',
-            module: webpackConfig.module,
-            resolve: webpackConfig.resolve
+            module: {
+                preLoaders: [
+                    {test: /\.js/, exclude: /(node_modules)/, loader: 'babel'},
+                    {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+                    {test: /\.(glsl|vs|fs)$/, loader: 'shader'},
+                    {test: /\.js/, include: path.resolve('src/'), loader: 'isparta'}
+                ]
+            },
+            resolve: webpackConfig.resolve,
+            isparta: {
+                embedSource: true,
+                noAutoWrap: true,
+                babel: {
+                    presets: ['es2015']
+                }
+            }
         },
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
