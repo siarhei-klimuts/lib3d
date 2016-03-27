ee8129764d22883c5ef31011acad6c8d8c0d8a32
Siarhei Klimuts <galiaf47@gmail.com>
2016-03-27 20:50:19 +0300

    ci firefox test

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,7 @@ var coverageReporter = {
 };
 var browsers = [];
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'test') {
     reporters.push('coveralls');
     browsers.push('Firefox');
 } else {
