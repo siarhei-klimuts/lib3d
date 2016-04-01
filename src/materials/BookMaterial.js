2eefe7e0b88c8464868a45ca97364cb77824cdfe
Siarhei Klimuts <galiaf47@gmail.com>
2016-04-01 16:57:43 +0300

    BookMaterial updated to use three.js r71 shader chunks

--- src/materials/BookMaterial.js
+++ src/materials/BookMaterial.js
@@ -1,7 +1,73 @@
 import THREE from 'three';
 
-var vertexShader = require('./shaders/BookMaterial.vs');
-var fragmentShader = require('./shaders/BookMaterial.fs');
+// var vertexShader = require('./shaders/BookMaterial.vs');
+// var fragmentShader = require('./shaders/BookMaterial.fs');
+
+var vertexShader = `
+varying vec3 vViewPosition;
+varying vec3 vNormal;
+${THREE.ShaderChunk.common}
+${THREE.ShaderChunk.map_pars_vertex}
+${THREE.ShaderChunk.lights_phong_pars_vertex}
+${THREE.ShaderChunk.color_pars_vertex}
+
+void main() {
+	${THREE.ShaderChunk.map_vertex}
+	${THREE.ShaderChunk.color_vertex}
+	${THREE.ShaderChunk.defaultnormal_vertex}
+	vNormal = normalize(transformedNormal);
+	${THREE.ShaderChunk.default_vertex}
+	vViewPosition = -mvPosition.xyz;
+	${THREE.ShaderChunk.worldpos_vertex}
+	${THREE.ShaderChunk.lights_phong_vertex}
+}`;
+
+var fragmentShader = `
+uniform vec3 diffuse;
+uniform float opacity;
+uniform vec3 emissive;
+uniform vec3 specular;
+uniform float shininess;
+uniform sampler2D coverMap;
+
+${THREE.ShaderChunk.common}
+${THREE.ShaderChunk.color_pars_fragment}
+${THREE.ShaderChunk.map_pars_fragment}
+${THREE.ShaderChunk.fog_pars_fragment}
+${THREE.ShaderChunk.lights_phong_pars_fragment}
+${THREE.ShaderChunk.bumpmap_pars_fragment}
+${THREE.ShaderChunk.specularmap_pars_fragment}
+
+void main() {
+	vec3 outgoingLight = vec3(0.0);
+	vec4 diffuseColor = vec4(diffuse, opacity);
+	vec4 baseColor;
+	vec4 testcolor = vec4(1.0, 1.0, 1.0, 1.0);
+	float eps = 0.004;
+
+	#ifdef USE_MAP
+		baseColor = texture2D(map, vUv);
+	#else
+		baseColor = vec4(1.0, 1.0, 1.0, 1.0);
+	#endif
+	
+	#ifdef USE_COVER
+		vec4 coverColor = texture2D(coverMap, vUv * vec2(2.3, 1.3) - vec2(1.3, 0.3));
+		if(vUv.y > 0.23 && (vUv.x > 0.57 || (all(greaterThanEqual(baseColor,testcolor-eps)) && all(lessThanEqual(baseColor,testcolor+eps)))))
+			diffuseColor = coverColor;
+		else
+			diffuseColor = baseColor;
+	#else
+		diffuseColor = baseColor;
+	#endif
+
+	${THREE.ShaderChunk.specularmap_fragment}
+	${THREE.ShaderChunk.lights_phong_fragment}
+	${THREE.ShaderChunk.color_fragment}
+	${THREE.ShaderChunk.fog_fragment}
+
+	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+}`;
 
 export default class BookMaterial extends THREE.ShaderMaterial {
 	constructor(mapImage, bumpMapImage, specularMapImage, coverMapImage) {
@@ -21,7 +87,7 @@ export default class BookMaterial extends THREE.ShaderMaterial {
 			THREE.UniformsLib.lights
 		]);
 
-		uniforms.shininess = {type: 'f', value: 30};
+		uniforms.shininess = {type: 'f', value: 10};
 		defines.PHONG = true;
 
 		if(mapImage) {
