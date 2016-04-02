b73dd0cc2b9df7e77d954f9b87a1a2f1fd269da5
Siarhei Klimuts <galiaf47@gmail.com>
2016-04-02 22:09:03 +0300

    Updated BookMaterial shaders

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,9 @@ var config = {
             {test: /\.json/, loader: 'json'}
         ]
     },
+    glsl: {
+        chunkPath: path.join(NODE_MODULES, 'three/src/renderers/shaders/ShaderChunk')
+    },
     plugins: [],
     resolve: {
         root: path.join(__dirname, 'src')
