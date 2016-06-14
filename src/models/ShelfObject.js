f388175750c8bc1e4f1ce4932987cebb1f32787a
Siarhei Klimuts <galiaf47@gmail.com>
2016-06-14 20:40:16 +0300

    Invisible shelves instead of transparent
    fix for black windows behind shelves

--- src/models/ShelfObject.js
+++ src/models/ShelfObject.js
@@ -21,8 +21,7 @@ class ShelfObject extends BaseObject {
 		this.position.set(params.position[0], params.position[1], params.position[2]);
 		this.size = new THREE.Vector3(size[0], size[1], size[2]);
 		
-		this.material.transparent = true;
-		this.material.opacity = 0;
+		this.material.visible = false;
 	}
 
 	/** {string} 'ShelfObject' */
